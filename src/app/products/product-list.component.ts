2cd971644237c54064594206b8a457ae03a67f04
noskcajmas <sjackson864@outlook.com>
2019-04-01 22:16:24 +0100

    Updates

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
@@ -12,6 +11,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string {
@@ -43,7 +43,12 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe(
+            products => {
+                this.products = products
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );   
     }
 }
